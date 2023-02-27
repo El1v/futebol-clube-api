39a5220100813acaabda5ffed51d2474dcb8b2a7
Elivelton <eliveltonmn@outlook.com>
2023-02-27 17:55:58 -0300

    feat: cfg docker & req01

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,28 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Team extends Model {
+  declare readonly id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
